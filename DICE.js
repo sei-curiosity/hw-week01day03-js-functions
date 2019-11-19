02825d46f6b8e1d11861e7f299e1bed480a27e2c
AlBattalSara <YOUR GITHUB EMAIL>
2019-11-20 01:04:09 +0300

    My HomwWork3

---
+++ DICE.js
@@ -0,0 +1,33 @@
+// ROLLING DICE GAME 
+// choosing two numbers randomly from 1 to 6
+// sum the two integers that has been choosen randomly
+
+// rollDice()
+// if the player has roll the dice and the result become forexample 7
+//
+// we need to generate two numbers in order to be choosen randomly
+
+// User choice & The System Choice
+// system choose Random choice.
+
+
+
+ function RollDice()
+{
+RollDice = Math.floor( Math.Random() * 6 + 1 );
+
+
+console.log ( "You Roll the Dice "+ RollDice ); // Sum the numbers
+// retun Rolldice; or Retun the Sum of rollDice
+}
+RollDiceRollDice();
+/*
+let x; 
+function RollDice(X)
+{
+
+   // retun (x + RollDice);
+
+}
+*/
+

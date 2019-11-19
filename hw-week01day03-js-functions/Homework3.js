ebf5ca80fd1950fea863e641a27a21d04c1a2498
AlBattalSara <YOUR GITHUB EMAIL>
2019-11-20 01:16:26 +0300

    HomeWork03

---
+++ hw-week01day03-js-functions/Homework3.js
@@ -0,0 +1,34 @@
+// NOTE! I did not create a new file and call it response.
+// Just added them to the HomeWorks
+
+/*3 : 
+3.  Research 3 string methods and describe what they are used for and how to use them.  You can find string methods here https://www.w3schools.com/js/js_string_methods.asp
+
+Method 1
+
+The Length
+
+Name: length.
+Description: It returns the total index number of a string.
+Example using it:
+var StudentName= "Haifa";
+var TheLength=Student.length;
+```
+
+Method 2
+```
+Name: search()
+Description: it retuns the index or the position of the string
+Example using it:
+var SName ="Haifa Albattal";
+var i = SName.search("AlBattal");
+
+Method 3
+```
+Name: "The Last Index" lastIndexOf()
+Description: it returns the last index/position of a string
+Example using it:
+var SName="Haifa AlBattal hold  the Firt Owner";
+var SName.lastIndexOf("Owner");
+```
+*/ 

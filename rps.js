5ee50ee9858a21026ae81141f4c1ae3c1495e6f2
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2019-11-20 06:31:04 +0300

    Rock, Paper, Scissors

---
+++ rps.js
@@ -0,0 +1,48 @@
+const getWinner = function(userShape, computerShape) {
+  let winner = `Computer picked: ${computerShape}. User picked: ${userShape}. `;
+
+  if (userShape === computerShape) {
+    return "Tie.";
+  }
+
+  if (userShape === "rock") {
+    if (computerShape === "paper") {
+      winner += "Computer won!";
+    } else {
+      winner += "User won!";
+    }
+  }
+
+  if (userShape === "paper") {
+    if (computerShape === "scissors") {
+      winner += "Computer won!";
+    } else {
+      winner += "User won!";
+    }
+  }
+
+  return winner;
+};
+
+const getComputerShape = function() {
+  const computerShapeIndex = Math.round(Math.random() * 2);
+
+  if (computerShapeIndex === 0) {
+    return "rock";
+  } else if (computerShapeIndex === 1) {
+    return "paper";
+  } else if (computerShapeIndex === 2) {
+    return "scissors";
+  } else {
+    return computerShapeIndex;
+  }
+};
+
+const userChoice = function(shape) {
+  const computerShape = getComputerShape();
+
+  const winner = getWinner(shape, computerShape);
+  console.log(winner);
+};
+
+userChoice("paper");
